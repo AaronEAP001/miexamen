3e9f76eabc494dd479ffcb6c0b300e72993079e5
Efrain <efrain_aaron@hotmail.com>
2021-12-08 18:24:50 -0500

    :'v

--- app/src/main/java/com/example/miexamen/BeginningFrame.java
+++ app/src/main/java/com/example/miexamen/BeginningFrame.java
@@ -8,9 +8,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class BeginningFrame extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.beginning_frame,container,false);
+        View view = inflater.inflate(R.layout.beginning_frame,container,false);
+        MaterialButton loginButton = view.findViewById(R.id.login_button);
+
+        //Accion para a
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((NavigationHost)getActivity()).navigateTo(new Login_Frame(),false);
+            }
+        });
+        return view;
+
     }
 }
--- app/src/main/java/com/example/miexamen/Login_Frame.java
+++ app/src/main/java/com/example/miexamen/Login_Frame.java
@@ -1,6 +1,16 @@
 package com.example.miexamen;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Login_Frame extends Activity {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class Login_Frame extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.login_frame,container,false);
+    }
 }
