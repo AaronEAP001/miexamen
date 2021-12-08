519b36a053d8ee4e977646acf8698c2f993adf07
Efrain <efrain_aaron@hotmail.com>
2021-12-08 01:47:06 -0500

    [one Frame - correction] finish frame for the beginning

--- app/src/main/java/com/example/miexamen/BeginningFrame.java
+++ app/src/main/java/com/example/miexamen/BeginningFrame.java
@@ -1,10 +1,16 @@
 package com.example.miexamen;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class BeginningFrame extends Fragment {
-
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.beginning_frame,container,false);
+    }
 }
