d603dc7b21d4f96b0ec1ac7b3f4bdb8e90edfc8a
Efrain <efrain_aaron@hotmail.com>
2021-12-08 01:12:37 -0500

    [one Frame] finish frame for the beginning

---
+++ app/src/main/java/com/example/miexamen/BeginningFrame.java
@@ -0,0 +1,10 @@
+package com.example.miexamen;
+
+import android.app.Activity;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public class BeginningFrame extends Fragment {
+
+}
