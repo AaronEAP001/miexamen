999e9a4c6e4a1005eccae867b5525cfe82c32f9d
Efrain <efrain_aaron@hotmail.com>
2021-12-09 10:46:24 -0500

    new frame started by [BeginningFrame]

--- app/src/main/java/com/example/miexamen/BeginningFrame.java
+++ app/src/main/java/com/example/miexamen/BeginningFrame.java
@@ -17,8 +17,7 @@ public class BeginningFrame extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.beginning_frame,container,false);
         MaterialButton loginButton = view.findViewById(R.id.login_button);
-
-        //Accion para a
+        MaterialButton signupButton = view.findViewById(R.id.sign_up);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +25,13 @@ public class BeginningFrame extends Fragment {
                 ((NavigationHost)getActivity()).navigateTo(new Login_Frame(),false);
             }
         });
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((NavigationHost)getActivity()).navigateTo(new SignupFrame(),false);
+            }
+        });
+
         return view;
 
     }
